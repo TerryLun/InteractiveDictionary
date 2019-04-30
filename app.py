eb104cb1404dc7171ddcd0fdbe841651706e3ef3
TerryLun <tianweilun@yahoo.com>
2019-04-29 22:44:13 -0700

    0429

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def lookUp(word):    
+def lookUp(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys(),cutoff=0.8))>0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no. " % get_close_matches(word, data.keys())[0])
+        if yn.lower() == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn.lower() == "n":
+            return "The Word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
         return "The Word doesn't exist. Please double check it."
 
